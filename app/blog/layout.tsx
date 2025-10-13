03aaa19fe6a6d095fe7cdef7f67910d4ecea62db
vibeholmes <113710917+vibeholmes@users.noreply.github.com>
2025-10-13 07:14:24 -0500

    fix: copy url button position in blog pages

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -19,7 +19,7 @@ function CopyButton() {
         setText('Copied')
         navigator.clipboard.writeText(currentUrl)
       }}
-      className="font-base flex items-center gap-1 text-center text-sm text-zinc-500 transition-colors dark:text-zinc-400"
+      className="font-base flex cursor-pointer items-center gap-1 text-center text-sm text-zinc-500 transition-colors dark:text-zinc-400"
       type="button"
     >
       <TextMorph>{text}</TextMorph>
@@ -43,10 +43,10 @@ export default function LayoutBlogPost({
         }}
       />
 
-      <div className="absolute top-24 right-4">
-        <CopyButton />
-      </div>
       <main className="prose prose-gray prose-h4:prose-base dark:prose-invert prose-h1:text-xl prose-h1:font-medium prose-h2:mt-12 prose-h2:scroll-m-20 prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-h4:font-medium prose-h5:text-base prose-h5:font-medium prose-h6:text-base prose-h6:font-medium prose-strong:font-medium mt-24 pb-20">
+        <div className="absolute top-38 left-4">
+          <CopyButton />
+        </div>
         {children}
       </main>
     </>
